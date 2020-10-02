40c38edb44a9d6896445d1fc762c62da309e94d9
duui <67356442+Duui3111@users.noreply.github.com>
2020-10-01 23:06:01 -0400

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -11,8 +11,8 @@ int main()
     char op; 
     cin >> num1 >> op >> num2;
 
-    if(op == '+') cout << num1 + num2 << endl; 
-    else if (op == '-') cout << "result " <<num1 - num2 << endl; 
+    if(op == '+') cout << "result " << num1 + num2 << endl; 
+    else if (op == '-') cout << "result " << num1 - num2 << endl; 
     else if (op == '*') cout << "results " << num1 * num2 << endl; 
     else if (op == '/') cout << "results " << num1 / num2 << endl; 
 
