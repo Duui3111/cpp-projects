016973c2d4f8edd9fa7be32961ee84c0f7feb0f9
duui <67356442+Duui3111@users.noreply.github.com>
2020-11-14 15:00:17 -0500

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -16,6 +16,6 @@ int main()
     else if (op == '*') cout << "results " << num1 * num2 << endl; 
     else if (op == '/') cout << "results " << num1 / num2 << endl; 
 
-    system("pause");
+    sin.get();
     return 0;
 } 
